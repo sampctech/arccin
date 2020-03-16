a6a0430f21b7659fda3c5de26d1c60398a28b304
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-16 03:59:39 -0400

    Add files via upload

--- AUR/add-broadsoft-wifi-v1.sh
+++ AUR/add-broadsoft-wifi-v1.sh
@@ -5,7 +5,5 @@
 #####################################################################
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #####################################################################
-./AUR/add-aic94xx-firmware-v1.sh
-./AUR/add-b43-firmware-v1.sh
-./AUR/add-broadsoft-wifi-v1.sh
-./AUR/add-wd719x-firmware-v1.sh
+
+sudo pacman -S b43-fwcutter
--- impset.sh
+++ impset.sh
@@ -6,18 +6,29 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #####################################################################
 ./execute.sh
-echo "Import or BackUP?" [i/b/n]
+echo "Have to run the Arco Tweak Tool & add repo's?" [y/n]
 read input
+if [[ "$input" == "n" ]]; then
+  echo "Import/BackUP bash & conky or add missing firware?" [i/b/f]
+  read input
     if [[ "$input" == "i" ]]; then
-    ./Personal/import.sh
-  elif [[ "$input" == "b" ]]; then
-    ./Personal/bkup.sh
-  elif [[ "$input" == "n" ]]; then
-    echo "Add missing firmware?" [y/n]
-    read input
-      if [[ "$input" == "y" ]]; then
-        ./AUR/firmware.sh
-      elif [[ "$input" == "n" ]]; then
-        exit
-      fi
+      ./Personal/import.sh
+    elif [[ "$input" == "b" ]]; then
+      ./Personal/bkup.sh
+    elif [[ "$input" == "f" ]]; then
+      ./AUR/firmware.sh
+      exit
+    fi
+elif [[ "$input" == "y" ]]; then
+  sudo pacman -Sy
+  echo "Import/BackUP bash & conky or add missing firware?" [i/b/f]
+  read input
+    if [[ "$input" == "i" ]]; then
+      ./Personal/import.sh
+    elif [[ "$input" == "b" ]]; then
+      ./Personal/bkup.sh
+    elif [[ "$input" == "f" ]]; then
+      ./AUR/firmware.sh
+      exit
+    fi
 fi
