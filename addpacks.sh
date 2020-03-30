c07a525d19644c4e9de8c48232c84af8d41d1462
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-03-29 23:03:35 -0400

    Create addpacks.sh
    updated list of packages to auto install with sudo pacman -S --noconfirm --needed

---
+++ addpacks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set -e
+#####################################################################
+# Author  : Sam Barna
+#####################################################################
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#####################################################################
+
+Programming
+sudo pacman -S --noconfirm --needed atom
+
+Internet
+sudo pacman -S --noconfirm --needed deluge
+
+Utilities
+sudo pacman -S --noconfirm --needed octopi
