7a06491511066bb8f83fab43dc74c35af242da7c
Sam Barna <54453917+sampctech@users.noreply.github.com>
2020-07-10 13:48:15 -0400

    Update import.sh
    simplified script

--- Personal/import.sh
+++ Personal/import.sh
@@ -6,17 +6,6 @@
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #####################################################################
 
-echo "Bashrc, Conky's, or All?" [b/c/a]
-read input
-  if [[ "$input" == "a" ]]; then
-    cp -fr "$HOME"/github/arccin/Personal/bashrc "$HOME"/.bashrc
-    cp -fr "$HOME"/github/arccin/Personal/myup.sh "$HOME"/.config/
-    cp -fr "$HOME"/github/arccin/Personal/mcy/conkyrc "$HOME"/.conkyrc
-    cp -fr "$HOME"/github/arccin/Personal/mcy/*.conkyrc "$HOME"/.config/conky
-  elif [[ "$input" == "b" ]]; then
-    cp -fr "$HOME"/github/arccin/Personal/bashrc "$HOME"/.bashrc
-    cp -fr "$HOME"/github/arccin/Personal/myup.sh "$HOME"/.config/
-  elif [[ "$input" == "c" ]]; then
-    cp -fr "$HOME"/github/arccin/Personal/mcy/conkyrc "$HOME"/.conkyrc
-    cp -fr "$HOME"/github/arccin/Personal/mcy/*.conkyrc "$HOME"/.config/conky
-fi
+cp -fr "$HOME"/github/arccin/Personal/.bashrc-personal "$HOME"/.bashrc-personal
+cp -fr "$HOME"/github/arccin/Personal/mcy/*.conkyrc "$HOME"/.config/conky
+cp -fr "$HOME"/.config/conky/AUR-ArcoLinux-SB.conkyrc "$HOME"/.conkyrc
